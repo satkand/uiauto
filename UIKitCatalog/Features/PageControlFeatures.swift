b628c3b2a4d78233b784e6198e2852a57dc40c2f
Simon Smiley-Andrews <>
2018-08-07 11:06:31 +1000

    Configure tests for tapping the sides of the pageControl

--- UIKitCatalog/Features/PageControlFeatures.swift
+++ UIKitCatalog/Features/PageControlFeatures.swift
@@ -1,9 +1,29 @@
-//
-//  PageControlFeatures.swift
-//  Features
-//
-//  Created by Simon Smiley-Andrews on 7/8/18.
 //  Copyright © 2018 Apple. All rights reserved.
-//
 
-import Foundation
+import XCTest
+
+final class PageControlFeatures: XCTestCase {
+  
+  private var application: XCUIApplication!
+  private let pageControlIdentifier: String = "page_control"
+  
+  override func setUp() {
+    super.setUp()
+    
+    application = XCUIApplication()
+    application.launch()
+    
+    application.tap(.cell, atIndex: 5)
+  }
+  
+  func testPageControlTaps() {
+    
+    application.verifyValue(expectedValue: "page 3 of 10", inElement: .pageControl, withIdentifier: pageControlIdentifier)
+    
+    application.tap(.pageControl, withIdentifier: pageControlIdentifier, withLocationArea: .left)
+    application.verifyValue(expectedValue: "page 2 of 10", inElement: .pageControl, withIdentifier: pageControlIdentifier)
+    
+    application.tap(.pageControl, withIdentifier: pageControlIdentifier, withLocationArea: .right)
+    application.verifyValue(expectedValue: "page 3 of 10", inElement: .pageControl, withIdentifier: pageControlIdentifier)
+  }
+}

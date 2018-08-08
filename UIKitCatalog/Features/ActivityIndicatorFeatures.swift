6ca80f0f6d1d473619b592bcf14577f179924e6d
Michael Loo <michael.loo@outware.com.au>
2018-08-08 14:48:56 +1000

    Test activity indicator exists

--- UIKitCatalog/Features/ActivityIndicatorFeatures.swift
+++ UIKitCatalog/Features/ActivityIndicatorFeatures.swift
@@ -18,7 +18,7 @@ final class ActivityIndicatorFeatures: XCTestCase {
     application.tap(element: .init(type: .cell, identifier: "cell_activity_indicator"))
 
     // Test activity indicator exists and is visible
-    application.expect(element: .init(type: .activityIndicator, identifier: "activity_indicator_gray"), to: .beVisible(true))
+    application.expect(element: .init(type: .activityIndicator, identifier: "activity_indicator_gray"), to: .exist(true))
     application.expect(element: .init(type: .activityIndicator, identifier: "activity_indicator_gray"), to: .beVisible(true))
 
     // Test activity indicator does not exists and is not visible

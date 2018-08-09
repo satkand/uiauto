f86ef7c0b63e52b426c5ad27fa9b623528932595
Simon Smiley-Andrews <>
2018-08-09 16:27:27 +1000

    Update returned comment about accessibility value for read text function

--- Source/Extensions/XCUIApplication+Read.swift
+++ Source/Extensions/XCUIApplication+Read.swift
@@ -19,7 +19,7 @@ extension XCUIApplication {
   ///     - file: the file in which failure occurred. Defaults to the file name of the test case in which this function was called.
   ///     - line: the line number on which failure occurred. Defaults to the line number on which this function was called.
   ///
-  /// - returns: Accessibility label of element
+  /// - returns: the accessibility value of an element if found, otherwise the accessibility label
   public func readText(from element: Element, file: StaticString = #file, line: UInt = #line) -> String? {
     let element: XCUIElement = first(element: element, file: file, line: line)
 

17a169bb5b9934200a390c0a0fa06cd5e3fdcf40
Michael Loo <michael.loo@outware.com.au>
2018-08-13 12:16:11 +1000

    Swipe to stepper cell before tapping on it

--- UIKitCatalog/Features/StepperFeatures.swift
+++ UIKitCatalog/Features/StepperFeatures.swift
@@ -13,6 +13,7 @@ final class StepperFeatures: XCTestCase {
     application = XCUIApplication()
     application.launch()
 
+    application.swipe(to: 12, in: "catalog_table", direction: .up)
     application.tap(element: .init(type: .cell, index: 12))
   }
 

ceee90ffc6258a28797492762e0c3416f2605e01
Andy Chen <andy.chen@outware.com.au>
2018-08-20 10:40:06 +1000

    Remove ability for user to select a specific photo from the library
    The function is suppose to be a high-level function that just selects a
    photo from the library without caring specifically about which photo is
    selected.

--- Source/Extensions/XCUIApplication+Photos.swift
+++ Source/Extensions/XCUIApplication+Photos.swift
@@ -4,26 +4,25 @@ import XCTest
 
 extension XCUIApplication {
 
-  /// Selects a photo from the library. By default, it will select the first photo in the "Camera Roll" album. If the permission has not been handled yet,
-  /// it will grant the access and continue selecting the photo.
+  /// Accepts photo library permission if required and selects a photo from the library.
   ///
   /// - Parameters:
-  ///   - album: the album containing the photo to be selected
-  ///   - photoIndex: the index representing the photo to select
   ///   - timeout: the specified amount of time to wait for elements to exist
   ///   - file: the file in which failure occurred. Defaults to the file name of the test case in which this function was called
   ///   - line: the line number on which failure occurred. Defaults to the line number on which this function was called
-  public func selectPhoto(fromAlbum album: String = "Camera Roll", atIndex photoIndex: Int = 0, timeout: TimeInterval = 2, file: StaticString = #file, line: UInt = #line) {
+  public func selectPhoto(timeout: TimeInterval = 2, file: StaticString = #file, line: UInt = #line) {
     acceptPermissionIfRequired(for: .photos)
 
     // There seems to be an Apple bug where using `.firstMatch` makes the process crash. Therefore, since we cannot use `first` and other Robocop actions
     // which make use `.firstMatch`, we have to query for the element ourselves.
-    let album: XCUIElement = query(for: .cell)[album]
+    let albumName: String = "Camera Roll"
+    let album: XCUIElement = query(for: .cell)[albumName]
 
-    assert(album.waitForExistence(timeout: timeout), message: "No element \(ElementType.cell) with identifier '\(album)' found.", file: file, line: line)
+    assert(album.waitForExistence(timeout: timeout), message: "No element \(ElementType.cell) with identifier '\(albumName)' found.", file: file, line: line)
 
     album.tap()
 
+    let photoIndex: Int = 0
     let photo: XCUIElement = query(for: .cell).element(boundBy: photoIndex)
 
     assert(photo.waitForExistence(timeout: timeout), message: "No element \(ElementType.cell) at index '\(photoIndex)' found.", file: file, line: line)

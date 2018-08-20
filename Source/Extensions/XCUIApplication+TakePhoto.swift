059868fc35c62d2892f1d57aeeeaf8510d3d06f4
Michael Loo <michael.loo@outware.com.au>
2018-08-20 11:20:29 +1000

    Pass timeout, file and line when taking or cancelling photo

--- Source/Extensions/XCUIApplication+TakePhoto.swift
+++ Source/Extensions/XCUIApplication+TakePhoto.swift
@@ -15,7 +15,7 @@ extension XCUIApplication {
   ///   - file: the file in which failure occurred. Defaults to the file name of the test case in which this function was called
   ///   - line: the line number on which failure occurred. Defaults to the line number on which this function was called
   public func takePhoto(timeout: TimeInterval = 0, file: StaticString = #file, line: UInt = #line) {
-    acceptPermissionIfRequired(for: .camera)
+    acceptPermissionIfRequired(for: .camera, timeout: timeout, file: file, line: line)
 
     // The camera app needs to be able to set the focus, otherwise it won't be able to take a photo (i.e.: please don't shake the device like crayz)
     tap(element: .init(type: .button, identifier: "PhotoCapture"), timeout: 5, file: file, line: line)
@@ -35,7 +35,7 @@ extension XCUIApplication {
   ///   - file: the file in which failure occurred. Defaults to the file name of the test case in which this function was called
   ///   - line: the line number on which failure occurred. Defaults to the line number on which this function was called
   public func cancelTakingPhoto(timeout: TimeInterval = 0, file: StaticString = #file, line: UInt = #line) {
-    acceptPermissionIfRequired(for: .camera)
+    acceptPermissionIfRequired(for: .camera, timeout: timeout, file: file, line: line)
     tap(element: .init(type: .button, identifier: "Cancel"), timeout: timeout, file: file, line: line)
   }
 }

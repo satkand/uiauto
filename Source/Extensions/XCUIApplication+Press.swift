5644e984eb47496278e2f22a8404296d6eb91c3c
Michael Loo <michael.loo@outware.com.au>
2018-08-22 09:14:22 +1000

    Add press and hold function without draging

--- Source/Extensions/XCUIApplication+Press.swift
+++ Source/Extensions/XCUIApplication+Press.swift
@@ -30,4 +30,22 @@ extension XCUIApplication {
 
     startCoordinate.press(forDuration: duration, thenDragTo: endCoordinate)
   }
+
+  /// Initiates a press-and-hold gesture.
+  ///
+  /// - Parameters:
+  ///   - element: the element to apply the gesture to
+  ///   - duration: the duration of the press-and-hold gesture
+  ///   - timeout: the specified amount of time to wait for the element to exist
+  ///   - file: the file in which failure occurred. Defaults to the file name of the test case in which this function was called
+  ///   - line: the line number on which failure occurred. Defaults to the line number on which this function was called
+  public func press(
+    element: Element,
+    forDuration duration: TimeInterval,
+    timeout: TimeInterval = 0,
+    file: StaticString = #file,
+    line: UInt = #line
+  ) {
+    first(element: element, timeout: timeout, file: file, line: line).press(forDuration: duration)
+  }
 }
